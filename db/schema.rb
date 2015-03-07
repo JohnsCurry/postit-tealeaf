ea9d167cbfbef5f6f2940106b5d5ff6fa1794308
John Curry <john.steven.curry@gmail.com>
2015-03-07 01:31:01 -0800

    bcrypt is throwing error because I changed it's name like it told me to

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306061942) do
+ActiveRecord::Schema.define(version: 20150307070537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150306061942) do
 
   create_table "users", force: true do |t|
     t.string "username"
+    t.string "password_digest"
   end
 
 end
